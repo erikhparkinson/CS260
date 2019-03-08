4be5e1c47f1ad2d4c19e9b1d79328969df829226
erikhparkinson <erikhparkinson@byu.edu>
2019-03-08 14:49:07 -0700

    almost done

--- CreativeProject3/script.js
+++ CreativeProject3/script.js
@@ -1,63 +1,72 @@
 var app = new Vue({
   el: '#app',
   data: {
-    todos: [],
-    message: '',
-    show: 'all',
-    drag: {},
+    answer: '',
+    correct: 0,
+    feedback: '',
+    max_num: 12,
+    number1: 0,
+    number2: 0,
+    symbol: 'x',
+    start: false,
   },
 
   computed: {
-    activeTodos() {
-      return this.todos.filter(item => {
-        return !item.completed;
-      });
+    correct_answer() {
+      if(this.symbol === 'x') {
+        return (this.number1 * this.number2).toString();
+      }
+      else if(this.symbol === '+') {
+        return (this.number1 + this.number2).toString();
+      }
+      else {
+        return (this.number1 - this.number2).toString();
+      }
     },
-    filteredTodos() {
-      if (this.show === 'active')
-        return this.todos.filter(item => {
-         return !item.completed;
-        });
-      if (this.show === 'completed')
-        return this.todos.filter(item => {
-          return item.completed;
-         });
-      return this.todos;
+    notStarted() {
+      return !this.start;
     },
   },
-  
   methods: {
-    addItem() {
-      this.todos.push({text: this.message, completed:false});
-      this.message = '';
+    startTest() {
+      this.start = true;
+      this.generateProblem();
     },
-    deleteItem(item) {
-    var index = this.todos.indexOf(item);
-    if (index > -1)
-      this.todos.splice(index,1);
+    nextQuestion() {
+      this.correct = 0;
+      this.feedback = '';
+      this.answer = '';
+      this.generateProblem();
     },
-    showAll() {
-      this.show = 'all';
+    checkAnswer() {
+      if (this.answer === this.correct_answer) {
+        this.correct = true;
+        this.feedback = 'You got it!';
+      }
+      else {
+        this.correct = false;
+        this.feedback = 'Nope!';
+      }
     },
-    showActive() {
-      this.show = 'active';
+    getRandomInt(max) {
+      return Math.floor(Math.random() * (Math.floor(max)+1));
     },
-    showCompleted() {
-      this.show = 'completed';
+    getRandomSymbol() {
+      var num = Math.random();
+      if(num < 1/3) {
+        return '+';
+      }
+      else if(num > 2/3) {
+        return '-';
+      }
+      else {
+        return 'x';
+      }
     },
-    deleteCompleted() {
-      this.todos = this.todos.filter(item => {
-        return !item.completed;
-      });
-    },
-    dragItem(item) {
-      this.drag = item;
-    },
-    dropItem(item) {
-      const indexItem = this.todos.indexOf(this.drag);
-      const indexTarget = this.todos.indexOf(item);
-      this.todos.splice(indexItem,1);
-      this.todos.splice(indexTarget,0,this.drag);
+    generateProblem() {
+      this.number1 = this.getRandomInt(this.max_num);
+      this.number2 = this.getRandomInt(this.max_num);
+      this.symbol = this.getRandomSymbol();
     },
   }
 });
